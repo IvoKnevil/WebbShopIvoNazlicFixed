e7d20ba9ba1392416e23f7da6e71fbf2c7a95985
Ivo Nazlic <78ivonaz@gafe.molndal.se>
2021-04-07 23:53:59 +0200

    Add project files.

---
+++ WebbShopIvoNazlic/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebbShopIvoNazlic.Database;
+using WebbShopIvoNazlic.Models;
+
+namespace WebbShopIvoNazlic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // MockData.AddData();
+
+
+            // Console.WriteLine(WebbShopAPI.Login("TestCustomer", "Codic2021"));
+
+
+            // WebbShopAPI.Logout(2);
+
+
+            // foreach (var item in WebbShopAPI.GetCategories(2))
+            // {
+            //    Console.WriteLine(item);
+            // }
+
+
+            // foreach (var item in WebbShopAPI.GetCategories(2, "h"))
+            // {
+            //   Console.WriteLine(item);
+            // }
+
+
+
+            // foreach (var item in WebbShopAPI.GetCategory(2, 2))
+            // {
+            //     Console.WriteLine(item);
+            // }
+
+
+            // Console.WriteLine(WebbShopAPI.GetBook(2, 2));
+
+
+            // foreach (var item in WebbShopAPI.GetBooks(2, "ee"))
+            // {
+            //    Console.WriteLine(item);
+            // }
+
+
+
+            // foreach (var item in WebbShopAPI.GetAuthors(2, "king"))
+            // {
+            // Console.WriteLine(item);
+            // }
+
+
+
+            // foreach (var item in WebbShopAPI.GetAvailableBooks(2, 2))
+            // {
+            //    Console.WriteLine(item);
+            // }
+
+
+
+            // Console.WriteLine(WebbShopAPI.BuyBook(2, 5)); 
+
+
+
+            // Console.WriteLine(WebbShopAPI.Ping(2));
+
+
+            // Console.WriteLine(WebbShopAPI.Register("Test2", "gg2"));
+
+
+            // WebbShopAPI.Login("Administrator", "CodicRulez");
+
+
+            // Console.WriteLine(WebbShopAPI.AddBook(1, "Pride and Prejudice", "Jane Austen" , 350, 10));
+
+
+
+            // Console.WriteLine(WebbShopAPI.SetAmount(1, 2));
+
+
+
+            // foreach (var item in WebbShopAPI.ListUsers(1))
+            // {
+            //    Console.WriteLine(item);
+            // }
+
+
+            // foreach (var item in WebbShopAPI.FindUser(1, "a"))
+            // {
+            // Console.WriteLine(item);
+            // }
+
+
+
+            // Console.WriteLine(WebbShopAPI.UpdateBook(1, 3, "Testing testing", "Madeup Author" , 350));
+
+
+
+            // Console.WriteLine(WebbShopAPI.DeleteBook(1, 3));
+
+
+
+            // Console.WriteLine(WebbShopAPI.AddCategory(1, "Comedy"));
+
+
+
+            // Console.WriteLine(WebbShopAPI.AddBookToCategory(1, 5, 5));
+
+
+
+            // Console.WriteLine(WebbShopAPI.UpdateCategory(1, 5, "Romance"));
+
+
+
+
+            // Console.WriteLine(WebbShopAPI.DeleteCategory(1, 4));
+
+
+
+            // Console.WriteLine(WebbShopAPI.AddUser(1, "Pest", "Kolera"));
+
+
+
+            //  foreach (var item in WebbShopAPI.SoldItems(1))
+            //  {
+            //     Console.WriteLine(item);
+            //  }
+
+
+            // Console.WriteLine(WebbShopAPI.MoneyEarned(1));
+
+
+            // Console.WriteLine(WebbShopAPI.BestCustomer(1));
+
+
+            //Console.WriteLine(WebbShopAPI.Promote(1, 3));
+
+
+            //Console.WriteLine(WebbShopAPI.Demote(1, 3));
+
+
+            //Console.WriteLine(WebbShopAPI.ActivateUser(1, 5));
+
+
+            //Console.WriteLine(WebbShopAPI.InactivateUser(1, 3));
+
+        }
+    }
+}
